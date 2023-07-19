ff7b04d4e0ce6f7c9f86561a8ae532ac95e02446
rabiebn <rabie.bnmr@gmail.com>
2023-07-19 19:08:56 +0100

    0: Initial commit

---
+++ 0-bubble_sort.c
@@ -0,0 +1,44 @@
+#include "sort.h"
+
+/**
+ * swap - Swap position of 2 elements in an array.
+ *
+ * @x: element to swap with @y
+ * @y: element to swap with @x
+ */
+void swap(int *x, int *y)
+{
+	int tmp = *x;
+
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * bubble_sort - Bubble Sort.
+ *
+ * @array: array of elemnts to sort.
+ * @size: number of elements in @array.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	bool swapped;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = false;
+
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				swapped = true;
+				print_array(array, size);
+			}
+		}
+		if (swapped == false)
+			break;
+	}
+}
