c770bf22fa221a563eb6f8f0387f20c7db24d7d8
rabiebn <rabie.bnmr@gmail.com>
2023-07-23 14:44:01 +0100

    0: Big O

---
+++ 3-quick_sort.c
@@ -0,0 +1,17 @@
+#include "sort.h"
+
+/**
+ * quick_sort - Quick sort an array of ints.
+ * 
+ * @array: array of ints to sort;
+ * @size: number of elements in @array.
+*/
+void quick_sort(int *array, size_t size)
+{
+    size_t i, pivot = size / 2;
+
+    for (i = 0; i < size; i++)
+    {
+        
+    }
+}
