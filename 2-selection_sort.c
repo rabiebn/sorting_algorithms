12e40eb7885dc668847acbbd420c589f84c70cbf
rabiebn <rabie.bnmr@gmail.com>
2023-07-25 16:13:16 +0100

    2: check if array is NULL or size < 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -22,15 +22,21 @@ void swap(int *x, int *y)
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
+	int i, j, min;
 
-	for (i = 0; i < size; i++)
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i; j < size; j++)
+		for (j = i + 1; j < size; j++)
 			if (array[min] > array[j])
 				min = j;
-		swap(&array[min], &array[i]);
-		print_array(array, size);
+		if (array[min] != array[i])
+		{
+			swap(&array[min], &array[i]);
+			print_array(array, size);
+		}
 	}
 }
