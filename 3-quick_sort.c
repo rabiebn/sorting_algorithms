c79713aefd7e14257f6ccc6ec711d69f4237add7
rabiebn <rabie.bnmr@gmail.com>
2023-07-25 15:22:42 +0100

    newline Big O files

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,6 +1,31 @@
 #include "sort.h"
 
 /**
+ * partition - Desc
+ *
+ * @high: 
+ * @low:
+ * Return: 
+*/
+size_t partition(int *array, size_t high, size_t low)
+{
+    size_t i = low - 1, j;
+    int pivot = array[high];
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(array[i], array[j]);
+        }
+    }
+    return (i + 1);
+}
+
+// define another quick sort function that takes array, high and low as args.
+
+/**
  * quick_sort - Quick sort an array of ints.
  * 
  * @array: array of ints to sort;
@@ -8,10 +33,6 @@
 */
 void quick_sort(int *array, size_t size)
 {
-    size_t i, pivot = size / 2;
+    size_t pivot = partition(array, size - 1, 0);
 
-    for (i = 0; i < size; i++)
-    {
-        
-    }
 }
